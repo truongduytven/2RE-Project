5f08933f889145bded3bfb177fdc58649f5e10ba
Võ Nhật Trường Duy K17 <duyvntse170126@fpt.edu.vn>
2024-09-22 13:19:32 +0700

    feat: header footer

--- src/components/global/footer.tsx
+++ src/components/global/footer.tsx
@@ -1,6 +1,62 @@
+import { Facebook, Instagram, Mail, PhoneCall } from 'lucide-react'
+import { Button } from '../ui/button'
+import Container from './Container'
 
 export default function Footer() {
   return (
-    <div>footer</div>
+    <footer>
+      <Container>
+        <div className='w-full h-full pt-5'>
+          <div className='flex mt-6 gap-14 justify-between'>
+            <div className='flex flex-col items-center flex-1 gap-1'>
+              {/* <div className='text-2xl font-bold text-primary'>Customer Support</div> */}
+              <div className='flex gap-4 mt-10'>
+                <Button variant='ghost' size='icon'>
+                  <Facebook />
+                </Button>
+                <Button variant='ghost' size='icon'>
+                  <Instagram />
+                </Button>
+                <Button variant='ghost' size='icon'>
+                  <Mail />
+                </Button>
+                <Button variant='ghost' size='icon'>
+                  <PhoneCall />
+                </Button>
+              </div>
+            </div>
+
+            <div className='flex flex-col items-center flex-1 gap-1'>
+              {/* <div className='text-2xl font-bold text-primary'>Contact information</div> */}
+              <div className='flex flex-col items-start gap-4 mt-2'>
+                <div className='text-xs sm:text-base'>
+                  <strong>Support</strong>
+                </div>
+                <div className='text-xs sm:text-base'>Delivery & Payment</div>
+                <div className='text-xs sm:text-base'>Help & Support</div>
+                <div className='text-xs sm:text-base'>24/7H Services</div>
+              </div>
+            </div>
+
+            <div className='flex flex-col items-center flex-1 gap-1'>
+              {/* <div className='text-2xl font-bold text-primary'>Contact information</div> */}
+              <div className='flex flex-col items-start gap-4 mt-2'>
+                <div className='text-xs sm:text-base'>
+                  <strong>Infomation</strong>
+                </div>
+                <div className='text-xs sm:text-base'>+8496 416 0769</div>
+                <div className='text-xs sm:text-base'>2re2hand@gmail.com</div>
+                <div className='text-xs sm:text-base'>
+                  <a href='/'>https://2re.vercel.app/</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Container>
+      <div className='flex justify-center items-center text-base h-10 mt-10'>
+        © Copyright 2024. All rights reserved.
+      </div>
+    </footer>
   )
 }
--- src/components/global/header.tsx
+++ src/components/global/header.tsx
@@ -1,6 +1,50 @@
+import Container from './Container'
+import Logo from '@/assets/Logo.jpeg'
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+} from '@/components/ui/navigation-menu'
+import { Button } from '../ui/button'
+import { Search } from 'lucide-react'
 
 export default function Header() {
   return (
-    <div>header</div>
+    <header className='w-full'>
+      <Container>
+        <div className='flex w-full h-20 justify-between items-end'>
+          <div>
+            <img src={Logo} className='w-30 h-20' />
+          </div>
+          <div>
+            <NavigationMenu>
+              <NavigationMenuList className='flex gap-6 justify-center items-center mb-2'>
+                <NavigationMenuItem className='relative after:absolute after:bg-gray-500 after:h-0.5 after:w-0 after:left-0 after:-bottom-1 after:hover:w-full after:duration-300'>
+                  <NavigationMenuLink href='/'>Home</NavigationMenuLink>
+                </NavigationMenuItem>
+                <NavigationMenuItem className='relative after:absolute after:bg-gray-500 after:h-0.5 after:w-0 after:left-0 after:-bottom-1 after:hover:w-full after:duration-300'>
+                  <NavigationMenuLink href='/shop'>Shop</NavigationMenuLink>
+                </NavigationMenuItem>
+                <NavigationMenuItem className='relative after:absolute after:bg-gray-500 after:h-0.5 after:w-0 after:left-0 after:-bottom-1 after:hover:w-full after:duration-300'>
+                  <NavigationMenuLink href='/product'>Product</NavigationMenuLink>
+                </NavigationMenuItem>
+                <NavigationMenuItem className='relative after:absolute after:bg-gray-500 after:h-0.5 after:w-0 after:left-0 after:-bottom-1 after:hover:w-full after:duration-300'>
+                  <NavigationMenuLink href='/product'>Cart</NavigationMenuLink>
+                </NavigationMenuItem>
+              </NavigationMenuList>
+            </NavigationMenu>
+          </div>
+          <div className='flex gap-6 mb-2'>
+            <Button variant='outline' size='icon'>
+              <Search className='h-4 w-4' />
+            </Button>
+            <Button>
+              Sign In
+            </Button>
+          </div>
+        </div>
+      </Container>
+    </header>
   )
 }
