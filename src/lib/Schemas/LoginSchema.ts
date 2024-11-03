73cb6bb363ecaf9491271b23bb4abfe86031b045
Võ Nhật Trường Duy K17 <duyvntse170126@fpt.edu.vn>
2024-11-03 13:41:28 +0700

    password 5 legit

--- src/lib/Schemas/LoginSchema.ts
+++ src/lib/Schemas/LoginSchema.ts
@@ -2,7 +2,7 @@ import { z } from 'zod'
 
 export const loginSchema = z.object({
     email: z.string().email({ message: 'Email không hợp lệ'}),
-    password: z.string().min(6, { message: 'Mật khẩu có ít nhất 6 kí tự' }),
+    password: z.string().min(5, { message: 'Mật khẩu có ít nhất 5 kí tự' }),
 })
 
 export const registerSchema = z.object({
@@ -11,7 +11,7 @@ export const registerSchema = z.object({
     address: z.string().min(2, { message: 'Địa chỉ có ít nhất 2 kí tự' }),
     email: z.string().email(),
     phoneNumber: z.string().startsWith('0', {message: 'Số điện thoại nên bắt đầu bằng 0'}).min(10, { message: 'Số điện thoại phải có 10 chữ số' }).max(10, { message: 'Số điện thoại phải có 10 chữ số' }),
-    password: z.string().min(6, { message: 'Mật khẩu có ít nhất 6 kí tự' }),
+    password: z.string().min(5, { message: 'Mật khẩu có ít nhất 5 kí tự' }),
     confirmPassword: z.string(),
 }).refine((data) => data.password === data.confirmPassword, {
     message: "Xác nhận mật khẩu không khớp",
