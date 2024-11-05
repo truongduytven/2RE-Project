60027b4af16c7f9d7f1295a3aad1165c27fff52c
Võ Nhật Trường Duy K17 <duyvntse170126@fpt.edu.vn>
2024-11-05 21:30:40 +0700

    QR

--- src/pages/PaymentSuccess.tsx
+++ src/pages/PaymentSuccess.tsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom'
 import { useEffect } from 'react'
+import Frame from '../assets/frame.png'
 function PaymentSuccess() {
   useEffect(() => {
     const scrollToTop = () => {
@@ -14,9 +15,12 @@ function PaymentSuccess() {
   return (
     <div className='w-screen flex justify-center items-center mb-8'>
       <div className='flex flex-col items-center'>
+        <div className='flex items-center gap-x-10'>
         <img src='https://i.pinimg.com/564x/35/cc/c9/35ccc963f1e758a6c7b1136ba883e6fc.jpg' className='my-10 w-[300px] h-[300px]' />
+        <img src={Frame} className='my-10 w-[300px] h-[350px]' />
+        </div>
         <div className='text-2xl font-medium'>Thanh toán thành công</div>
-        <div className='text-xl mt-4'>Vui lòng kiểm tra đơn hàng trong đơn hàng của tôi hoặc trong email!!!</div>
+        <div className='text-xl mt-4'>Bạn hãy quét mã QR và để lại đánh giá mua hàng để giúp 2RE ngày càng phát triển hơn nha!!</div>
         <p className='text-lg mt-4'>
           Cảm ơn vì đã tin tưởng <span className='text-primary font-medium'>2RE Secondhand Fashion</span>
         </p>
