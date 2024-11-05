1b70e35d7e5b109dffeff5ad44d0ccc12b14e461
Võ Nhật Trường Duy K17 <duyvntse170126@fpt.edu.vn>
2024-11-05 13:41:33 +0700

    fix: color text

--- src/components/global/rootLayout.tsx
+++ src/components/global/rootLayout.tsx
@@ -4,7 +4,7 @@ import { Outlet } from 'react-router-dom'
 
 export default function RootLayout() {
   return (
-    <div className='h-screen relative bg-teriary text-[#ae9079]'>
+    <div className='h-screen relative bg-teriary text-[#5E3023]'>
       <Header />
       <div className='min-h-[70%] pt-10 mt-24 bg-teriary'>
         <Outlet />
